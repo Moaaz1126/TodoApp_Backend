259b62175de92edf293f7f532af8cb38fd342bf9
Moaaz1126 <videotextwep@gmail.com>
2023-09-20 03:47:07 -0700

    Server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 app.use('*', (req, res, next) => {
   console.log(req.path, req.method)
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
